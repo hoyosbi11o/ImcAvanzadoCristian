14a725b6b555dc6612ad7c0f6f01274e14377f39
angel <hoyosbolivara11@gmail.com>
2023-11-12 12:56:15 -0500

    my first commit

---
+++ TrabajoChenao/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+				
+		VentanaPrincipal miVentana=	new VentanaPrincipal();
+		miVentana.setVisible(true);
+		
+
+	}
+	
+
+}
